c290371c5f74b7a433ed2524c9eb14fd568b9714
Wayne <Wayne@WaynedeMac-mini.local>
2014-10-29 00:15:20 +0800

    a tab example

---
+++ Pad Shot/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Pad Shot
+//
+//  Created by Wayne on 14/10/29.
+//  Copyright (c) 2014年 Wayne. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
