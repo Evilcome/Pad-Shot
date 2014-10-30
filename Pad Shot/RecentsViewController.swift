b087011443d2b98f802e48f867e8ca0761f45625
Wayne <Wayne@WaynedeMac-mini.local>
2014-10-30 23:25:00 +0800

    add nav controller

---
+++ Pad Shot/RecentsViewController.swift
@@ -0,0 +1,13 @@
+//
+//  RecentsViewController.swift
+//  Pad Shot
+//
+//  Created by Wayne on 14/10/30.
+//  Copyright (c) 2014年 Wayne. All rights reserved.
+//
+
+import UIKit
+
+class RecentsViewController: UITableViewController {
+
+}
